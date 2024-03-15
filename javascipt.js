c9c0f424e39c5aed40ef9097860f81dc6fc683f3
MateusL21 <mateusleonel27@gmail.com>
2024-03-15 12:18:36 -0300

    closes #5 - botao calcular

--- javascipt.js
+++ javascipt.js
@@ -1,6 +1,10 @@
 var prods = [
-    { id: 1, name: "Produto 1", price: 10.0 },
-    { id: 2, name: "Produto 2", price: 20.0 },
+    { id: 1, name: "Bife com Batata", price: 30.0 },
+    { id: 2, name: "Coxa de Frango crocante", price: 25.0 },
+    { id: 3, name: "Carne de Panela", price: 22.0 },
+    { id: 4, name: "Farofa", price: 10.0 },
+    { id: 5, name: "Salada", price: 8.0 },
+    { id: 6, name: "Torresmo", price: 12.0 },
 ];
 
 function calc(){
